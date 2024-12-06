aa33a7b0f713882258726bb0d8c937047cae4280
Jaophat / เจ้าภัทร <jaophat.oct@gmail.com>
2024-12-06 14:11:37 +0700

    Add files via upload

---
+++ Homework1/Homework1_1/Homework1_1.cpp
@@ -0,0 +1,13 @@
+/*
+จงเขียนโปรแกรมภาษา C ที่มีโครงสร้างครบถ้วน โดยประกอบไปด้วยการ include และ main function (ชนิดข้อมูล int) 
+เพื่อแสดงข้อความ "Hello World." บนหน้าจอ 
+*/
+
+#include <stdio.h>
+
+int main( ) { 
+
+    printf( "Hello World." ) ;
+    return 0 ;
+
+} // end main
---
+++ Homework1/Homework1_2/Homework1_2.cpp
@@ -0,0 +1,21 @@
+/*จงแก้ Bug ของโปรแกรมดังต่อไปนี้ เพื่อให้โปรแกรมสามารถรันได้ตามปกติ
+
+#include <studio .h>
+int main() 
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) 
+    printf( "This is my first time to coding with C language.\n" ) ;
+    print( "Today is my birthday.\n" ) 
+    return '0' ;
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ; 
+    printf( "This is my first time to coding with C language.\n" ) ;
+    printf( "Today is my birthday.\n" ) ; 
+    
+    return 0 ;
+
+} // end main
